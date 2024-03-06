8ae9fa5811f2a781c12df296b5abcc861b825adc
ValentinCoccimiglio <91237369+ValentinCoccimiglio@users.noreply.github.com>
2024-03-06 16:33:20 -0300

    Se cargo el turno de la computadora

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,8 +12,11 @@ let puntosJugador = 0,
 
 //Refes de html
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+
 const puntosHtml = document.querySelectorAll('small');
-const divCartasJugador = document.querySelector('#jugador-carta')
+const divCartasJugador = document.querySelector('#jugador-carta');
+const divCartasComputadora = document.querySelector('#computadora-carta')
 
 
 //Crea nuevo deck
@@ -54,10 +57,48 @@ const valorCarta = ( carta ) => {
             ( valor === 'A') ? 11 : 10
             : valor * 1;
 }
+//Turno computadora
+
+const turnoComputadora = (puntosMinimos) => {
+
+    do {
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHtml[1].innerText = puntosComputadora;
+        
+        
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cards/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+
+}
+
+
+
 
 
 //Eventos
-btnPedir.addEventListener('click', function() {
+btnPedir.addEventListener('click', () => {
 
     const carta = pedirCarta();
 
@@ -72,12 +113,20 @@ btnPedir.addEventListener('click', function() {
     if (puntosJugador > 21) {
         console.warn('Te pasaste de 21');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        divCartasJugador.append (imgCarta)
     
 
     } else if (puntosJugador === 21) {
         console.warn('21, Genial!');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+    
     }
 
 
+});
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
 });
