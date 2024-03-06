e0d22f361a1c14bd61766e6f55918de51fe638a6
ValentinCoccimiglio <91237369+ValentinCoccimiglio@users.noreply.github.com>
2024-03-06 15:19:52 -0300

    Se le da valor a las cartas

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,4 +40,13 @@ const pedirCarta = () => {
     console.log(carta);
     return carta;
 };
+
 pedirCarta(); 
+
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+        return( isNaN( valor )) ? 
+            ( valor === 'A') ? 11 : 10
+            : valor * 1;
+}
+const valor = valorCarta ( perdirCarta() );
